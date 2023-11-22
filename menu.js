3521b8822858c0176e93f4fb17fd91317c361c9f
Jérôme Roussel <74191010+jelepotier@users.noreply.github.com>
2023-11-22 17:07:15 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,20 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('nav');
 
     menuIcon.addEventListener('click', function () {
+        console.log('Menu icon clicked');
         nav.classList.toggle('show');
+        console.log('Is menu now open?', nav.classList.contains('show'));
     });
 
     document.addEventListener('click', function (event) {
         if (!nav.contains(event.target) && nav.classList.contains('show')) {
+            console.log('Document clicked; hiding menu');
             nav.classList.remove('show');
         }
     });
 
-    // Ajoutez cette partie pour gérer la fermeture du menu lorsque vous cliquez sur un lien
     const navLinks = document.querySelectorAll('nav a');
 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
+            console.log('Link clicked; hiding menu');
             nav.classList.remove('show');
         });
     });
