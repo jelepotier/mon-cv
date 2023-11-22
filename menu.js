44435570523bac3d0efbccbc7a31e3fb699589de
Jérôme Roussel <74191010+jelepotier@users.noreply.github.com>
2023-11-22 10:59:35 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const nav = document.querySelector('nav');
 
+    menuIcon.addEventListener('click', function () {
+        nav.classList.toggle('show');
+    });
+
     document.addEventListener('click', function (event) {
-        if (event.target === menuIcon) {
-            nav.classList.toggle('show');
-        } else if (!nav.contains(event.target)) {
+        if (!nav.contains(event.target) && !menuIcon.contains(event.target)) {
             nav.classList.remove('show');
         }
     });
