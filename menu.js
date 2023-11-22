4061d804ea72d2171233bbbe69b027c583233a26
Jérôme Roussel <74191010+jelepotier@users.noreply.github.com>
2023-11-22 16:58:22 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,18 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const nav = document.querySelector('nav');
 
-    console.log(menuIcon); // Vérifie si l'élément est correctement sélectionné
-
     menuIcon.addEventListener('click', function () {
-        console.log('Menu icon clicked'); // Vérifie si l'événement de clic est détecté
         nav.classList.toggle('show');
-        console.log(nav.classList.contains('show')); // Vérifie si la classe 'show' est ajoutée ou retirée
     });
 
     document.addEventListener('click', function (event) {
         if (!nav.contains(event.target) && nav.classList.contains('show')) {
             nav.classList.remove('show');
-            console.log('Document clicked; hiding menu'); // Vérifie si le document est cliqué lorsque le menu est ouvert
+        } else if (event.target.tagName === 'A') {
+            // Si un lien est cliqué, fermez le menu
+            nav.classList.remove('show');
         }
     });
 });
