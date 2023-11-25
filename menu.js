301e97fa817dd253cc823ab5f1c09739b3a5e3fc
jelepotier <jejelarifle@gmail.com>
2023-11-25 13:37:27 +0100

    modifie le menu.js

--- menu.js
+++ menu.js
@@ -1,13 +1,15 @@
-// Récupérer le bouton du menu
-var menuButton = document.querySelector('.menu-icon');
+document.addEventListener('DOMContentLoaded', function() {
+    // Récupérer le bouton du menu
+    var menuButton = document.querySelector('.menu-icon');
 
-// Récupérer la navigation
-var nav = document.querySelector('nav');
+    // Récupérer la navigation
+    var nav = document.querySelector('nav');
 
-// Ajouter un gestionnaire d'événement au clic sur le bouton du menu
-menuButton.addEventListener('click', function() {
-    // Toggle la classe 'show' sur la navigation
-    nav.classList.toggle('show');
+    // Ajouter un gestionnaire d'événement au clic sur le bouton du menu
+    menuButton.addEventListener('click', function() {
+        // Toggle la classe 'show' sur la navigation
+        nav.classList.toggle('show');
+    });
 
     // Ajouter un gestionnaire d'événement pour fermer le menu lorsqu'un lien est cliqué
     var navLinks = document.querySelectorAll('nav ul li a');
