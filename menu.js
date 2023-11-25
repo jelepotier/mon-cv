1ae94388cacad7b9498d142ab0d8a52ad6320207
jelepotier <jejelarifle@gmail.com>
2023-11-25 12:39:41 +0100

    rélolution menu burger

--- menu.js
+++ menu.js
@@ -1,13 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var menuIcon = document.querySelector(".menu-icon");
-    var navList = document.querySelector("nav ul");
+// Récupérer le bouton du menu
+var menuButton = document.querySelector('.menu-icon');
 
-    menuIcon.addEventListener("click", function () {
-        // Basculez la visibilité des éléments du menu
-        if (navList.style.display === "block") {
-            navList.style.display = "none";
-        } else {
-            navList.style.display = "block";
-        }
+// Récupérer la navigation
+var nav = document.querySelector('nav');
+
+// Ajouter un gestionnaire d'événement au clic sur le bouton du menu
+menuButton.addEventListener('click', function() {
+    // Toggle la classe 'show' sur la navigation
+    nav.classList.toggle('show');
+
+    // Ajouter un gestionnaire d'événement pour fermer le menu lorsqu'un lien est cliqué
+    var navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            nav.classList.remove('show');
+        });
     });
 });
